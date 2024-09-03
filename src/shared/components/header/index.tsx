d5bd94fc2c54f4a30ee0702580a435d2fa9a84f7
andersonalves45 <anderson.oliveira@livsaude.com.br>
2024-09-03 05:30:39 -0300

    feat(Button): create custom button

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,17 +1,24 @@
 import { Bell, Calendar } from "lucide-react";
+import { Button } from "../ui/button";
 
 const Header = () => {
 	//mock
 	return (
-		<header className="bg-grayscale-light py-2 px-14 flex justify-between items-center">
+		<header className="bg-grayscale-light py-4 px-14 flex justify-between items-center">
 			<h1 className="text-text-secondary font-bold">TODOLIST</h1>
-			<div>input</div>
+			<input type="text" />
 			<div className="flex gap-2 items-center">
-				<Bell />
-				<Calendar />
+				<Button size="sm">
+					<Bell size={18}/>
+				</Button>
+				<Button size="sm">
+					<Calendar size={18}/>
+				</Button>
 				<div className="ml-4">
-					<p className="text-text-primary">Terça-feira</p>
-					<span className="text-secondary-brand">03/09/2024</span>
+					<p className="text-text-primary font-semibold">Terça-feira</p>
+					<span className="text-secondary-brand font-semibold text-sm">
+						03/09/2024
+					</span>
 				</div>
 			</div>
 		</header>
