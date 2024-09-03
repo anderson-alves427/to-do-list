928ed55520a514bab6a7e72d71402a52df0bbbc1
andersonalves45 <anderson.oliveira@livsaude.com.br>
2024-09-03 04:34:20 -0300

    config(Tailwind): config tailwind in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          brand: "#bef264",
+        },
+        body: {
+          success: "#7FB431",
+          warning: "#B2343A",
+        },
+        grayscale: {
+          black: "#09090b",
+          white: "#FFFFFF",
+          stroke: "#18181b",
+        },
+        text: {
+          primary: "#FAFAFA",
+          secondary: "#A2A1A8",
+          tertiary: "#D4D4D8",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
