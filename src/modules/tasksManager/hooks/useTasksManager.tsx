0b4f225727cfcb64c9b5136926c9aacd7e1150fd
andersonalves45 <anderson.oliveira@livsaude.com.br>
2024-09-06 08:57:37 -0300

    refactor: dnd

---
+++ src/modules/tasksManager/hooks/useTasksManager.tsx
@@ -0,0 +1,93 @@
+import { useUserContext } from "@/shared/contexts/user/useUserContext";
+import { useTasksContext } from "../context/useTaskContext";
+import { useEffect } from "react";
+
+function useTasksManager() {
+	const { getTasks, groupWithTasks, getTasksGroup, groups } = useTasksContext();
+	const { user } = useUserContext();
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function onDragEnd(result: any) {
+		const { destination, source } = result;
+
+		// Se não houver destino ou o destino é o mesmo local de origem, não fazer nada
+		if (!destination) {
+			return;
+		}
+
+		// Caso o item foi movido para a mesma posição ou lista
+		if (
+			destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		) {
+			return;
+		}
+
+		// Encontrar a lista de origem e destino
+		const start = groupWithTasks.value.data.find(
+			(list) => list.id === source.droppableId
+		);
+		const finish = groupWithTasks.value.data.find(
+			(list) => list.id === destination.droppableId
+		);
+		console.log("---->", start);
+		if (!start || !finish) return;
+		// Se a lista de origem e destino são a mesma, mover o item dentro da mesma lista
+		if (start === finish) {
+			const newTasks = Array.from(start.tasks);
+			const [movedTask] = newTasks.splice(source.index, 1);
+			newTasks.splice(destination.index, 0, movedTask);
+
+			const newList = { ...start, tasks: newTasks };
+			const newState = groupWithTasks.value.data.map((list) =>
+				list.id === newList.id ? newList : list
+			);
+			groupWithTasks.set({
+				page: groupWithTasks.value.page,
+				data: newState,
+				size: groupWithTasks.value.size,
+				total: groupWithTasks.value.total,
+			});
+			return;
+		}
+
+		// Mover o item entre listas diferentes
+		const startTasks = Array.from(start.tasks);
+		const [movedTask] = startTasks.splice(source.index, 1);
+
+		const newStart = { ...start, tasks: startTasks };
+		const newFinishTasks = Array.from(finish.tasks);
+		newFinishTasks.splice(destination.index, 0, movedTask);
+		const newFinish = { ...finish, tasks: newFinishTasks };
+
+		const newState = groupWithTasks.value.data.map((list) => {
+			if (list.id === newStart.id) {
+				return newStart;
+			}
+			if (list.id === newFinish.id) {
+				return newFinish;
+			}
+			return list;
+		});
+
+		groupWithTasks.set({
+			page: groupWithTasks.value.page,
+			data: newState,
+			size: groupWithTasks.value.size,
+			total: groupWithTasks.value.total,
+		});
+	}
+
+	useEffect(() => {
+		getTasksGroup();
+		getTasks(user.value.id);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return {
+		groupWithTasks,
+		groups,
+		onDragEnd,
+	};
+}
+
+export { useTasksManager };
